1d44db05e834ee8a34d3ae9f097b3a168716bf4d
HENRY ELUEME <juniortelli22@gmail.com>
2023-10-15 06:12:25 +0100

    fetch stores

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -5,9 +5,9 @@ import { Store } from "@prisma/client";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Check, ChevronsUpDown, Store as StoreIcon } from "lucide-react";
+import { Check, ChevronsUpDown, PlusCircle, Store as StoreIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from "@/components/ui/command";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>;
 interface StoreSwitcherProps extends PopoverTriggerProps {
@@ -37,35 +37,41 @@ export default function StoreSwitcher({ className, items = [] }: StoreSwitcherPr
       <PopoverTrigger asChild>
         <Button variant="outline" size="sm" aria-expanded={open} aria-label="Select a store" className={cn("w-[200px] justify-between", className)}>
           <StoreIcon className="mr-2 h-4 w-4" />
-          Current Store
-          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50"/>
+          {currentStore?.label}
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
         </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-[200px] p-0">
-              <Command>
-                  <CommandList>
-                      <CommandInput placeholder="Search store..." />
-                      <CommandEmpty>
-                          No store found.
-                      </CommandEmpty>
-                      <CommandGroup heading="Stores">
-                          {formattedItems.map((store) => (
-                              <CommandItem
-                                  key={store.value}
-                                  onSelect={() => onStoreSelect(store)}
-                                  className="text-sm"
-                              >
-                                  <StoreIcon className="mr-2 h-4 w-4" />
-                                  {store.label}
-                                  <Check
-                                  className={cn("ml-auto h-4w-4", currentStore?.value === store.value ? "opacity-100" : "opacity-0")}
-                                  />
-                              </CommandItem>
-                          ))}
-                      </CommandGroup>
-                  </CommandList>
-              </Command>
-          </PopoverContent>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandInput placeholder="Search store..." />
+            <CommandEmpty>No store found.</CommandEmpty>
+            <CommandGroup heading="Stores">
+              {formattedItems.map((store) => (
+                <CommandItem key={store.value} onSelect={() => onStoreSelect(store)} className="text-sm">
+                  <StoreIcon className="mr-2 h-4 w-4" />
+                  {store.label}
+                  <Check className={cn("ml-auto h-4w-4", currentStore?.value === store.value ? "opacity-100" : "opacity-0")} />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+          <CommandSeparator />
+          <CommandList>
+            <CommandGroup>
+              <CommandItem
+                onSelect={() => {
+                  setOpen(false)
+                  storeModel.onOpen()
+              }}
+              >
+                <PlusCircle className="mr-2 h-5 w-5" />
+                Create Store
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
     </Popover>
   );
 }
